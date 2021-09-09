a76f215b3698d358639b9eddaf42e6363a5a3544
Noah Allan Ertz <naertz@dmacc.edu>
2021-09-08 19:43:07 -0500

    Initial commit

---
+++ measurements.h
@@ -0,0 +1,16 @@
+// =========================
+// Name    : Measurements
+// Author  : Noah Allan Ertz
+// Created : 09/08/2021
+// =========================
+
+#ifndef MEASUREMENTS_H
+#define MEASUREMENTS_H
+
+class Measurements {
+    public:
+        virtual double perimeter() = 0; // Pure Virtual Perimeter Calculator
+        virtual double area() = 0;      // Pure Virtual Area Calculator
+};
+
+#endif // MEASUREMENTS_H
